bb0670a27c60b706b20b9b0ee151226da7acdc81
Akash Das <akashdas02052@gmail.com>
2026-05-05 17:35:02 +0530

    Update LeadData.model.js

--- models/LeadData.model.js
+++ models/LeadData.model.js
@@ -7,16 +7,19 @@ const leadSchema = new mongoose.Schema({
   },
   name: {
     type: String,
-    required: true,
+    required: [true, "name is required and must be a string."],
   },
   status: {
     type: String,
     enum: ["New", "Contacted", "Qualified", "Proposal Sent", "Closed", "Lost"],
-    required: true,
+    required: [
+      true,
+      "status must be one of New, Contacted, Qualified, Proposal Sent, Closed, Lost.",
+    ],
   },
   salesAgent: {
     type: mongoose.Schema.Types.ObjectId,
-    required: true,
+    required: [true, "salesAgent must be a valid ObjectId."],
   },
   source: {
     type: String,
@@ -32,24 +35,28 @@ const leadSchema = new mongoose.Schema({
       `Google`,
       `Other`,
     ],
-    required: true,
+    required: [
+      true,
+      "source is required and must be one of the predefined values (`Website`, `YouTube`, `Instagram`, `Facebook`, `News paper`, `Our Customers`, `Referral`, `Television Ad`,`Google`,`Other`).",
+    ],
   },
   phoneNumber: {
     type: String,
-    required: true,
+    required: [true, "Phone number is required."],
   },
   tags: {
     type: String,
     enum: [`High Value`, `Follow Up`],
-    required: true,
+    required: [true, "Tag is required."],
   },
   timeToClose: {
     type: Number,
+    require: [true, "timeToClose must be a positive integer btw 0 to 30."],
     default: null,
   },
   priority: {
     type: String,
-    required: true,
+    required: [true, "priority must be one of High, Medium, Low."],
   },
   closedAt: {
     type: String,
