7c847abe9cf42c8b52f0de1259bbf2248bfc40d9
Akash Das <akashdas02052@gmail.com>
2026-05-08 12:50:05 +0530

    Update SalesAgent.model

--- models/SalesAgent.model.js
+++ models/SalesAgent.model.js
@@ -4,24 +4,35 @@ const salesAgentSchema = new mongoose.Schema(
   {
     agentCode: {
       type: String,
-      required: true,
-      unique: true,
+      required: [true, "agentCode must exist!"],
+      unique: [true, "agentCode must be unique!"],
+      match: [/^AG-\d+$/, 'agentCode must be in format like "AG-6426045967"'],
     },
     name: {
       type: String,
-      required: true,
+      required: [true, "name is required."],
+      match: [
+        /^[A-Za-z]+(?: [A-Za-z]+)+$/,
+        "Enter name in format like 'John Doe'",
+      ],
     },
     dateOfBirth: {
       type: String,
-      required: true,
+      required: [true, "DOB is required."],
+      match: [/^\d{4}-\d{2}-\d{2}$/, "DOB must be in format yyyy-mm-dd"],
     },
     country: {
       type: String,
-      required: true,
+      required: [true, "Country is required."],
+      match: [/^[A-Z]/, "First letter must be capital"],
     },
     phoneNumber: {
       type: String,
-      required: true,
+      required: [true, "Phone number is required."],
+      match: [
+        /^\(\+\d{1,3}\)\d+$/,
+        "Phone number must be in format like (+91)9865327541",
+      ],
     },
     phoneNumberNormalized: {
       type: String,
@@ -29,44 +40,57 @@ const salesAgentSchema = new mongoose.Schema(
     },
     email: {
       type: String,
-      required: true,
-      unique: true,
-      lowercase: true,
+      required: [true, "email is required."],
+      unique: [true, "email must be unique."],
+      lowercase: [true, "every character in email must be in lower case."],
+      match: [
+        /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+        "email must have '@' and '.' characters",
+      ],
+    },
+    address: {
+      type: String,
+      required: [true, "address is required field"],
     },
     profileImg: {
       type: String,
-      required: true,
+      required: [true, "Profile image is required field"],
     },
     role: {
       type: String,
-      required: true,
+      required: [true, "role is required."],
       default: "Sales Agent",
     },
     manager: {
       type: mongoose.Schema.Types.ObjectId,
-      required: true,
-      default: "69eafb7ba796c8a7b4a87429",
+      required: [true, "manager is required."],
     },
     location: {
       type: String,
-      required: true,
+      required: [true, "location is required."],
       default: "Office",
     },
     department: {
       type: String,
-      required: true,
+      required: [true, "department is required."],
       default: "Sales",
     },
     joinedDate: {
       type: String,
-      required: true,
-      required: true,
+      required: [true, "mention join date."],
+      match: [/^\d{4}-\d{2}-\d{2}$/, "joinedDate must be in format yyyy-mm-dd"],
     },
     status: {
       type: String,
       enum: ["Active", "Inactive"],
-      required: true,
+      required: [true, "status is required"],
       default: "Inactive",
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      unique: [true, "Password must be unique"],
+      minlength: [6, "Password must contain at least 6 characters"],
     },
   },
   { timestamps: true },
