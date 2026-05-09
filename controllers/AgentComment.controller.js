12a67a6d7d30141732f3e98115a641cfc61e8acc
Akash Das <akashdas02052@gmail.com>
2026-05-09 13:12:53 +0530

    Feat: Implement custom error handler on agent comment data routes

--- controllers/AgentComment.controller.js
+++ controllers/AgentComment.controller.js
@@ -1,45 +1,9 @@
-import LeadModel from "../models/LeadData.model.js"
 import {
   createComment,
   getCommentsById,
 } from "../services/AgentComment.service.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
 
-export const addComment = async (req, res) => {
-  try {
-    const { id: leadId } = req.params
-    const { commentText, author } = req.body
+export const addComment = asyncHandler(createComment)
 
-    const lead = await LeadModel.findById(leadId)
-    if (!lead) {
-      return res.status(404).json({ message: "Lead not found" })
-    }
-
-    const comment = {
-      lead: leadId,
-      author: author,
-      commentText,
-    }
-
-    const Comment = await createComment(comment)
-
-    res.status(200)
-    res.json(Comment)
-  } catch (err) {
-    res.status(500)
-    res.json({ message: err.message })
-  }
-}
-
-export const getCommentsByLeadId = async (req, res) => {
-  try {
-    const { id: leadId } = req.params
-
-    const comments = await getCommentsById(leadId)
-
-    res.status(200)
-    res.json(comments)
-  } catch (err) {
-    res.status(500)
-    res.json({ message: err.message })
-  }
-}
+export const getCommentsByLeadId = asyncHandler(getCommentsById)
--- services/AgentComment.service.js
+++ services/AgentComment.service.js
@@ -1,19 +1,55 @@
 import AgentCommentModel from "../models/AgentComment.model.js"
+import LeadModel from "../models/LeadData.model.js"
+import { NotFoundError, ValidationError } from "../utils/customErrorHandler.js"
 
-export const createComment = async (comment) => {
+export const createComment = async (req, res) => {
   try {
+    const { id: leadId } = req.params
+    const { commentText, author } = req.body
+
+    const lead = await LeadModel.findById(leadId)
+    if (!lead) {
+      throw new NotFoundError("Lead not found!")
+    }
+
+    const comment = {
+      lead: leadId,
+      author: author,
+      commentText,
+    }
+
     const Comment = await AgentCommentModel.create(comment)
-    return Comment
+
+    res.status(200)
+    res.json(Comment)
   } catch (error) {
-    throw error
+    if (error.kind === "ObjectId") {
+      throw new ValidationError("Id must be a valid ObjectId")
+    } else {
+      throw error
+    }
   }
 }
 
-export const getCommentsById = async (leadId) => {
+export const getCommentsById = async (req, res) => {
   try {
+    const { id: leadId } = req.params
+
+    const lead = await LeadModel.findById(leadId)
+    console.log(lead)
+    if (!lead) {
+      throw new NotFoundError("Lead not found!")
+    }
+
     const comment = await AgentCommentModel.find({ lead: leadId })
-    return comment
+
+    res.status(200)
+    res.json(comment)
   } catch (error) {
-    throw error
+    if (error.kind === "ObjectId") {
+      throw new ValidationError("Id must be a valid ObjectId")
+    } else {
+      throw error
+    }
   }
 }
